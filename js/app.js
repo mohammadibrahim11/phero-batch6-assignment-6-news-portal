d55a6988054a1a5ead5cb14a45a1e9b32aab9b6d
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-09-04 02:41:35 +0600

    news catetgory added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
 
-const newsInACategory = () =>{
-    const url =`https://openapi.programming-hero.com/api/news/category/01`
-    fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data.data))
-}
-
-
 // get caegory url
 const newsCategoris = () =>{
     url = `https://openapi.programming-hero.com/api/news/categories`
@@ -29,3 +21,35 @@ const displayCategories =(news_category)=>{
  }
 }
 newsCategoris();
+
+
+
+
+const newsInACategory = () =>{
+    const url =`https://openapi.programming-hero.com/api/news/category/01`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayNews(data.data))
+}
+
+const displayNews = newses =>{
+    const newsContainer = document.getElementById('news-container');
+
+    for(const news of newses ){
+     console.log(news.data);
+    
+    newsContainer.innerHTML =`
+    <div class="col-md-4">
+    <img src="..." class="img-fluid rounded-start" alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>`
+
+}
+}
+newsInACategory();
+
